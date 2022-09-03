46ec85437a34b5f1621aba473d5a76222060ef72
gmendozatello31 <gmendozatello31>
2022-09-03 05:25:32 +0000

    asd
    asd

--- framework_landing/saps4/extraction_landing.py.py
+++ framework_landing/saps4/extraction_landing.py.py
@@ -1,2 +1,17 @@
 # Databricks notebook source
+# Databricks notebook source
+from common_function.general_functions import max_file_storage
 
+# COMMAND ----------
+
+import sys 
+my_working_path ="/Workspace/alicorp/common_function"
+if my_working_path not in sys.path:
+    sys.path.append(my_working_path)
+
+print (sys.path)    
+
+
+# COMMAND ----------
+
+from common_function import 
