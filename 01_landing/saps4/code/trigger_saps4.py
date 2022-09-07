65f6d7123ba2d28213c880f6645d918de5581b51
gmendozatello31 <gmendozatello31>
2022-09-07 15:44:25 +0000

    version5
    version5

--- 01_landing/saps4/code/trigger_saps4.py
+++ 01_landing/saps4/code/trigger_saps4.py
@@ -3,9 +3,20 @@
 
 # COMMAND ----------
 
+# MAGIC %run ../cssssssssssssssssssssssssssssssssssssssssssssssssssssssssss/utils
+
+# COMMAND ----------
+
+import sys
+sys.path.append("Worspace/Repos/main/alicorp")
+print(sys.path)
+
+# COMMAND ----------
+
 import json
+
 # JSON file
-f = open ('./config.json', "r")
+f = open ('config.json', "r")
 #Reading from file
 data = json.loads(f.read())
 # Iterating through the json
@@ -32,14 +43,5 @@ a = '''   { "tables": [
 y = json.loads(a)
 print("JSON string = ", y)
 
-# JSON file
-#f = open ('data.json', "r")
-# Reading from file
-#data = json.loads(f.read())
-# Iterating through the json
-# list
-#for i in data['emp_details']:#
-#	print(i)
-# Closing file
-#f.close()
-
+for i in y['tables']:#
+	print(i)
