7a116845ad110f1e9adccd55707910ac3ac9200e
gmendozatello31 <gmendozatello31>
2022-09-08 20:49:08 +0000

    prueba
    prueba

---
+++ prueba_databricks.py
@@ -0,0 +1,19 @@
+# Databricks notebook source
+from utils.sample2 import cube_root
+cube_root(8)
+
+# COMMAND ----------
+
+from utils.utils_tempo import cube_root
+cube_root(8)
+
+
+# COMMAND ----------
+
+pip install xgboost
+
+# COMMAND ----------
+
+import sys
+print("\n".join(sys.path))
+
--- utils/utils_tempo.py
+++ utils/utils_tempo.py
@@ -1,2 +1,2 @@
 def cube_root(n):
-  print("The cube root of", n, "is", n**(1.0/3))
+    print("The cube root of", n, "is", n**(1.0/3))
