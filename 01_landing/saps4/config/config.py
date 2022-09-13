e1500a0b016822a9a51ef0e4f757adb7fd14315d
gmendozatello31 <gmendozatello31>
2022-09-13 21:08:37 +0000

    version final
    version final

--- 01_landing/saps4/config/config.py
+++ 01_landing/saps4/config/config.py
@@ -1,31 +1,4 @@
 # Databricks notebook source
-import json 
-
-json_file = '''
-    [
-        { "name": "KNA1","partition_field": "D","reproces":"N","days":"0"},
-        { "name": "KNVH","partition_field": "D","reproces":"N","days":"0"},
-        { "name": "KNVV","partition_field": "D","reproces":"N","days":"0"},
-        { "name": "KNB1","partition_field": "D","reproces":"N","days":"0"},
-        { "name": "LIPS","partition_field": "D","reproces":"N","days":"0"},
-        { "name": "LIKP","partition_field": "D","reproces":"N","days":"0"},
-        { "name": "VBAP","partition_field": "D","reproces":"N","days":"20"},
-        { "name": "VBAK","partition_field": "D","reproces":"S","days":"0"},
-        { "name": "MARA","partition_field": "D","reproces":"N","days":"0"},
-        { "name": "MAKT","partition_field": "D","reproces":"N","days":"0"},
-        { "name": "TVFKT","partition_field": "M","reproces":"N","days":"0"}
-    ]
-    '''
-
-def conf_json ():
-    list_table = json.loads(json_file)
-    return list_table
-
-
-print("****** conf_json  *********")
-
-# COMMAND ----------
-
 json_file = '''
     [
         { "table": {
@@ -38,7 +11,7 @@ json_file = '''
                      },
          "schedule": {
                      "format":"daily",
-                     "day":["04","15","30","09"]
+                     "day":[]
                     }
          },
          { "table": {
